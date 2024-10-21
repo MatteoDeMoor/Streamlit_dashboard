5e67c6f574b680facbf9ac76c2bca5afb32843eb
Matteo De Moor <matteo.demoor@student.hogent.be>
2024-10-21 08:36:34 +0000

    Auto-sync dashboard from DEP-2324-G05

--- role_selection.py
+++ role_selection.py
@@ -41,7 +41,6 @@ def show_role_selection():
     with col3:
         st.empty()
 
-    st.markdown("<style>.st-emotion-cache-1tyc88k {display:none;}</style>", unsafe_allow_html=True)
 
     # Add white space
     st.write("##")
